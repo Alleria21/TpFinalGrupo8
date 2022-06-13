cb413887c10e0606296474172a364dbc806dd43c
Rocio <roaguerrero17@gmail.com>
2022-06-13 19:30:19 -0300

    Initial commit

---
+++ src/main/java/ar/edu/unju/fi/tpfinalgrupo8/TpFinalGrupo8Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinalgrupo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFinalGrupo8Application.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tpfinalgrupo8/TpFinalGrupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinalgrupo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalGrupo8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
